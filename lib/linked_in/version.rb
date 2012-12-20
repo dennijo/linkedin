555c08e67e43b936da274633d620c20681780b83
Josh Dennis <josh@joshdennis.net>
2012-12-20 15:13:35 -0600

    version bump

--- lib/linked_in/version.rb
+++ lib/linked_in/version.rb
@@ -3,8 +3,8 @@ module LinkedIn
   module VERSION #:nodoc:
     MAJOR = 0
     MINOR = 3
-    PATCH = 7
-    PRE   = 1
+    PATCH = 8
+    PRE   = nil
     STRING = [MAJOR, MINOR, PATCH, PRE].compact.join('.')
   end
 
